fc72cbea29ede1f61abda799617bb701495dc875
Danang-Bahtiar <dan.dev1903@gmail.com>
2025-09-19 15:04:35 +0700

    [Friday, 19-09-2025] - Resolve export problem.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as ChordJS } from "./core/discrafter.js";
-export { ChordJSConfig, defineConfig } from "./config/discrafter.config.js";
+export { default as Discrafter } from "./core/discrafter.js";
+export { DiscrafterConfig, defineConfig } from "./config/discrafter.config.js";
 export { slashCommandTemplate } from "./template/slashCommand.template.js";
 export { eventTemplate } from "./template/event.template.js";
